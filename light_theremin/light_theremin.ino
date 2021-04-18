01124fc331fc5b63c6955b17fbf24125ea2dc9e1
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2021-04-18 21:26:34 +0300

    coded up, compiles

--- light_theremin/light_theremin.ino
+++ light_theremin/light_theremin.ino
@@ -1,6 +1,26 @@
-void setup() {
   // Light Theremin 
   // Project 5
+
+int sensorvalue;
+int sensorLow = 1023;
+int sensorHigh = 0;
+
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  while  (millis()  < 5000) {
+     int sensorValue = analogRead(A0);
+     if (sensorValue > sensorValue) {
+        sensorHigh = sensorValue;
+     }
+     if (sensorValue < sensorLow) {
+        sensorLow = sensorValue;
+  }
+  }
+  
 
 }
 
