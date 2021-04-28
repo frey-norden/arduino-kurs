d99eb2adc630f4874e931c228a8299b4d8b7b4e1
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2021-04-28 13:22:45 +0300

    esp32 autoconnect added

---
+++ loraNtp/LoRaNtp.ino
@@ -0,0 +1,16 @@
+#include <AsyncUDP.h>
+
+
+// helltec esp32 w LoRa
+// ntp over udp
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
