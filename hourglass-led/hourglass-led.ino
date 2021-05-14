545d8ea3f995272d00c829c2c97c09b568232246
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2021-05-14 15:05:12 +0300

    added main loop

--- hourglass-led/hourglass-led.ino
+++ hourglass-led/hourglass-led.ino
@@ -7,7 +7,7 @@ unsigned long previousTime = 0;
 int switchState = 0;
 
 int led = 2;
-long interval = 600000;
+long interval = 600000; // 10 mins in millis 10 * 60  secs
 
 
 void setup() {
@@ -22,6 +22,24 @@ void setup() {
 void loop() {
   // time for the loop int main void arg loopy loop
   unsigned  long currentTime = millis();
-  if(currentTime - previousTime > interval)
+  if(currentTime - previousTime > interval); {
+    previousTime = currentTime;
+    digitalWrite(led, HIGH);  // lights the pin
+    led++;
 
+    if(led ==  7){
+      // something happens at end of hour -- lightshow doors pink floyd?
+    }
+  }
+  switchState = digitalRead(switchPin);
+  if(switchState != prevSwitchState){
+    for(int x=2;x<8;x++){
+      digitalWrite(x, LOW);
+    }
+
+    led = 2;
+    previousTime = currentTime;
+  }
+  
+  prevSwitchState = switchState;
 }
