8993a526a3ba5a74161678f280c4ea3ab2af00a5
Jeffrey Jackson <jeffrey.jackson@tuni.fi>
2021-05-20 10:20:30 +0300

    setting up ultra ping

--- ultra_ranger/ultra_ranger.ino
+++ ultra_ranger/ultra_ranger.ino
@@ -8,11 +8,24 @@ float timeOut = MAX_DISTANCE * 60;
 int soundVelocity = 340; // define speed of sound = 340m/s
 
 void setup() {
-  // put your setup code here, to run once:
+  // pins and serial connection
+  pinMode(trigPin, OUTPUT); // trigPin is output
+  pinMode(echoPin, INPUT);  // echoPin is input from ultrasound bounce
+  Serial.begin(9600);  // open serial monitor -- view ping results what optimus sees
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // int main void 
+  delay(100);  /* // Wait 100ms between pings (about 20 pings/sec). 29ms should be the
+shortest delay between pings. */
+  Serial.print('Ping: ');
+  Serial.print(getSonar());  // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.println('cm')
+}
 
+float getSonar() {
+  unsigned long pingTime;
+  float distance;
+  digitalWrite(trigPin, HIGH; // make trigPin output high level for 10 µs
 }
